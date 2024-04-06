5e9fc0775022bf4025667c8c15321f6d005421a2
Ramesh Babu Y <hotcoder@users.noreply.github.com>
2024-04-06 10:35:45 +0530

    Initial Commit

---
+++ src/main/java/com/rby/rbychatserver/RbychatserverApplication.java
@@ -0,0 +1,13 @@
+package com.rby.rbychatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbychatserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbychatserverApplication.class, args);
+	}
+
+}
