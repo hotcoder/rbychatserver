863669cf4d70503e325a94ea9f88911ee9fda2d9
Ramesh Babu Y <hotcoder@users.noreply.github.com>
2024-04-08 17:49:59 +0530

    Update UserService.java

--- src/main/java/com/rby/rbychatserver/service/UserService.java
+++ src/main/java/com/rby/rbychatserver/service/UserService.java
@@ -3,7 +3,7 @@ package com.rby.rbychatserver.service;
 import com.rby.rbychatserver.dto.UserDTO;
 import com.rby.rbychatserver.model.ChatUser;
 
-// UserService.java
+
 public interface UserService {
     ChatUser authenticate(String username, String password);
     boolean joinChatRoom(Long userId, Long roomId);
