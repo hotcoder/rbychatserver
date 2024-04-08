f59d5b863cda06f65150cd909b187138b436bba7
Ramesh Babu Y <hotcoder@users.noreply.github.com>
2024-04-08 17:49:38 +0530

    Update ChatService.java

--- src/main/java/com/rby/rbychatserver/service/ChatService.java
+++ src/main/java/com/rby/rbychatserver/service/ChatService.java
@@ -5,7 +5,7 @@ import com.rby.rbychatserver.model.ChatRoom;
 
 import java.util.List;
 
-// ChatService.java
+
 public interface ChatService {
     List<ChatMessage> getChatMessages(Long roomId);
     void saveMessage(ChatMessage message);
